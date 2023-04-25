c774f9c2780cc08cfee42f84e172db9c1bb41579
chipgentoo <chip.gentoo@gmail.com>
2023-04-26 01:48:32 +0300

    Семинар 6 Домашняя работа

--- HomeWorks/Sem_6_HW.py
+++ HomeWorks/Sem_6_HW.py
@@ -18,22 +18,22 @@
 Определить индексы элементов массива (списка), значения которых принадлежат заданному диапазону
 (т.е. не меньше заданного минимума и не больше заданного максимума)
 '''
-import random
-def gen_list(x): # Лениво вводить руками
-    in_list = []
-    for i in range(x):
-        in_list.append(random.randint(0,x))
-    return in_list
+# import random
+# def gen_list(x): # Лениво вводить руками
+#     in_list = []
+#     for i in range(x):
+#         in_list.append(random.randint(0,x))
+#     return in_list
 
-start = int(input('Введите начало диапазона: ') )
-stop = int(input('Введите конец диапазрна: ') )
+# start = int(input('Введите начало диапазона: ') )
+# stop = int(input('Введите конец диапазрна: ') )
 
-in_list = gen_list(30)
-print(in_list)
-idx_list = []
+# in_list = gen_list(30)
+# print(in_list)
+# idx_list = []
 
-for i in range(len(in_list)):
-    if start <= in_list[i] <= stop:
-        idx_list.append(i)
+# for i in range(len(in_list)):
+#     if start <= in_list[i] <= stop:
+#         idx_list.append(i)
 
-print(idx_list)
+# print(idx_list)
