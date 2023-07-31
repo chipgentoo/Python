64f3189926c3b8a101ff2bcb66f9778cc1f52a41
chipgentoo <chip.gentoo@gmail.com>
2023-07-31 11:40:20 +0300

    first commit

--- HomeWorks/Sem_8_HW.py
+++ HomeWorks/Sem_8_HW.py
@@ -1,21 +1,25 @@
 file_path = "tel_sprav.csv"
 
-def file_read(file_path):
-    with open(file_path, 'r+', encoding='utf-8') as file:
+
+def file_read(filepath):
+    with open(filepath, 'r+', encoding='utf-8') as file:
         return file.read()
 
-def showTel(file_path):
+
+def showtel(filepath):
     print('===========================================')
-    for record in file_read(file_path).split('\n'):
+    for record in file_read(filepath).split('\n'):
         print(record)
     print('===========================================')
+
 
 def rec_add():
     family = input("Введите Фамилию: ")
     name = input("Введите Имя: ")
     parent = input("Введите Отчество: ")
     telephone = input("Введите телефон: ")
-    return (family + ';' + name + ';' + parent + ';' + telephone + ';\n')
+    return family + ';' + name + ';' + parent + ';' + telephone + ';\n'
+
 
 def rec_find():
     print('===========================================')
@@ -23,19 +27,21 @@ def rec_find():
     for user in file_read(file_path).split(";"):
         for userDetail in user.split("\n"):
             if userDetail.lower() == element.lower():
-                print("Найдено: " +  user)
+                print("Найдено: " + user)
     print('===========================================')
+
 
 def rec_write():
     with open(file_path, 'a', encoding='utf-8') as file:
         file.write(rec_add())
 
-def Main():
-    showTel(file_path)
-    print('add\t - Добавить')
+
+def main():
+    showtel(file_path)
+    print('add \t - Добавить')
     print('find\t - Поиск')
-    print('end\t - Выход')
-    in_str = input()
+    print('end \t - Выход')
+    in_str = input(">>")
     if in_str == 'add':
         rec_write()
     elif in_str == 'find':
@@ -43,8 +49,9 @@ def Main():
     elif in_str == 'end':
         return 'end'
 
+
 while True:
-    if Main() == 'end':
+    if main() == 'end':
         break
     else:
-        Main()
+        main()
